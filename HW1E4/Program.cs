020685d180028cde3d6216fca7d9348e63db57cc
CyberPunk58 <pavelrip@mail.ru>
2020-09-25 12:50:43 +0300

    HW1

---
+++ HW1E4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1E4
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Андреев Павел
+             * Написать программу обмена значениями двух переменных:
+             *  а) с использованием третьей переменной;
+	         *  б) *без использования третьей переменной.
+             * 
+             * 
+             */
+
+            int a, b, c, d, e, f, g;
+            //С использованием третьей переменной.
+            a = 1;
+            b = 2;
+            c = a;
+            a = b;
+            b = c;
+           
+            Console.WriteLine($"a = {a}, b = {b}");
+            ///////////////
+            
+            //Без использования третьей, но для чисельных переменных.
+
+            d = 3;
+            e = 5;
+
+            d = d + e;
+            e = d - e;
+            d = d - e;
+
+            Console.WriteLine($"d = {d}, e = {e}");
+            ///////////////
+
+            //Еще без использования третьей переменной   
+            f = 10;
+            g = 20;
+
+            f ^= g;
+            g ^= f;
+            f ^= g;
+
+            Console.WriteLine($"f = {f}, g = {g}");
+        }
+    }
+}
---
+++ HW1E6/HW1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1E6
+{
+    public class Program
+    {
+        /**Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+         *  
+         */
+
+
+        //Создал метод MyPrint, подключил ссылку в HW1E5
+        public static void MyPrint(string t1) //Метод для печати текста
+        {
+            Console.WriteLine(t1);
+            //  return;
+        }
+
+
+        public static void Pause() //Метод для ожидания нажатия клавиши, после выполнения программы
+        {
+
+            Console.ReadKey();  
+        }
+        public static void Main()
+        {
+       
+        }
+
+    }
+}
